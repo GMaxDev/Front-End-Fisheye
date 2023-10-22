aa5f56727cf60e59253ad5fef5f54b36158db282
gmaxdev <maxime.guigra@gmail.com>
2023-10-22 15:05:06 +0200

    Préparation class pour medias

---
+++ scripts/models/MediasPhotographers.js
@@ -0,0 +1,39 @@
+class MediasPhotographers{
+    constructor(data) {
+        this._id = data.media.id
+        this._photographersId = data.media.photographersId
+        this._title = data.media.title
+        this._image = data.media.image
+        this._likes = data.media.likes
+        this._date = data.media.date
+        this._price = data.media.price
+    }
+
+    get id(){
+        return this._id
+    }
+
+    get photographersId(){
+        return this._photographersId
+    }
+
+    get title(){
+        return this._title
+    }
+
+    get image(){
+        return this._image
+    }
+
+    get likes(){
+        return this._likes
+    }
+
+    get date(){
+        return this._date
+    }
+
+    get price(){
+        return this._price
+    }
+}
