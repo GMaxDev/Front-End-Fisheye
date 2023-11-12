72c1a9627839bc03b5b64de319fc8ab44a81de3e
gmaxdev <maxime.guigra@gmail.com>
2023-11-12 12:01:46 +0100

    positionnemenet de la modal par-dessus avec background

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,13 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const overlay = document.getElementById("overlay");
+	modal.style.display = "flex";
+    overlay.style.display = "block"; // Affiche l'overlay
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const overlay = document.getElementById("overlay");
     modal.style.display = "none";
+    overlay.style.display = "none"; // Affiche l'overlay
 }
