426157a8ad6ab9350399fa8a443fb3bfbb80127e
gmaxdev <maxime.guigra@gmail.com>
2023-11-27 15:13:43 +0100

    fix responsive photograph-header

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,36 +1,36 @@
 // Ajoute l'exportation de photographerTemplate
 export function photographerTemplate(data) {
-    const { name, id, portrait, city, country, tagline, price } = data;
+  const { name, id, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'button' );
-        article.setAttribute("id", id)
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        const h3 = document.createElement( 'h3' )
-        const h4 = document.createElement( 'h4' )
-        const h5 = document.createElement( 'h5' )
-        h2.textContent = name;
-        h3.textContent = city + ', ' + country;
-        h4.textContent = tagline;
-        h5.textContent = price + '€/jour';
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(h4);
-        article.appendChild(h5);
+  function getUserCardDOM() {
+    const article = document.createElement("button");
+    article.setAttribute("id", id);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    const h4 = document.createElement("h4");
+    const h5 = document.createElement("h5");
+    h2.textContent = name;
+    h3.textContent = city + ", " + country;
+    h4.textContent = tagline;
+    h5.textContent = price + "€/jour";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(h5);
 
-        // J'ajoute un gestionnaire d'événements "click" à l'article
-        article.addEventListener('click', () => {
-            const clickedId = article.id;
-            const newPageURL = `/photographer.html?id=${clickedId}`;
-            window.location.href = newPageURL;
-        });
+    // J'ajoute un gestionnaire d'événements "click" à l'article
+    article.addEventListener("click", () => {
+      const clickedId = article.id;
+      const newPageURL = `/photographer.html?id=${clickedId}`;
+      window.location.href = newPageURL;
+    });
 
-        return article;
-    }
-    return { name, id, picture, city, country, tagline, price, getUserCardDOM };
+    return article;
+  }
+  return { name, id, picture, city, country, tagline, price, getUserCardDOM };
 }
